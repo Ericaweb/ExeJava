9b23a923cec3d950bd525fc30126801424a1f096
Érica Batista <ericabatista.web@gmail.com>
2025-03-08 19:42:55 -0300

    Add files via upload

---
+++ src/Pares.java
@@ -0,0 +1,9 @@
+public class Pares {
+    public static void main(String[] args) {
+        System.out.println("\nContando os valores par de 0 a 100.\n");
+
+        for (int i = 0; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
